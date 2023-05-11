8da0384a4ae8852226b7109e86b6de3a695b04fd
MOHD JUNAID ALI <junaidali3089@gmail.com>
2023-05-11 21:24:33 +0530

    added h1 and para

--- project2/src/App.js
+++ project2/src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Global warming</h1>
+      <p>Global warming is the long-term heating of Earth's surface observed since the pre-industrial period between 1850 and 1900 due to human activities, primarily fossil fuel burning, which increases heat-trapping greenhouse gas levels in Earth's atmosphere. This term is not interchangeable with the term "climate change."</p>
     </div>
   );
 }
